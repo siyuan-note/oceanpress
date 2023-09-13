18c5365b360bc1dc1939f5d4f981cf443b208ee5
崮生 <admin@shenzilong.cn>
2023-09-13 16:03:09 +0800

    test

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -2,4 +2,9 @@
   <h1>未开发完毕，暂不能使用</h1>
   <div>可关注 <a href="https://ld246.com/article/1693989505448"> OceanPress_js 版的开发记录 </a> </div>
   <router-view></router-view>
+  <div>
+    <div>由<a href="https://heartstack.space/user/%E5%AD%90%E8%99%9A">崮生（子虚</a>开发</div>
+    <div><a href="https://oceanpress-js.pages.dev">项目地址</a></div>
+    <div><a href="https://github.com/2234839/oceanPress_js">开源代码仓库</a></div>
+  </div>
 </template>
