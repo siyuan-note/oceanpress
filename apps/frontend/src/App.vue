31b4ef6dd9a7ebe3b0cd6f37cab279d8543751b5
崮生 <admin@shenzilong.cn>
2023-09-13 15:57:11 +0800

    test

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,3 +1,5 @@
 <template>
+  <h1>未开发完毕，暂不能使用</h1>
+  <div>可关注 <a href="https://ld246.com/article/1693989505448"> OceanPress_js 版的开发记录 </a> </div>
   <router-view></router-view>
 </template>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# OceanPress
+
+参见 [OceanPress_js 版的开发记录 ](https://ld246.com/article/1693989505448)
