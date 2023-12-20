cbe8abe9a4df700e0d01783657b281bba25efee9
崮生 <admin@shenzilong.cn>
2023-12-20 17:54:48 +0800

    清理无用依赖

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJSX from '@vitejs/plugin-vue-jsx'
 export default defineConfig({
-  plugins: [vue(), vueJSX()],
+  plugins: [vue()],
   build: {
     outDir: './dist/',
     emptyOutDir: true,
