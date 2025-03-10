3ee61df3ea1557698cf18190cd4f94cd43b67ac1
崮生（子虚） <2234839456@qq.com>
2025-03-10 22:51:23 +0800

    clean

--- apps/rpc/src/index.ts
+++ apps/rpc/src/index.ts
@@ -1,23 +1,2 @@
 import { createRPC } from './createRPC';
 export { createRPC };
-async function main() {
-  const apis = {
-    test() {
-      return 'Hello, world!';
-    },
-  };
-  const serverRPC = await createRPC('apiProvider' as const, {
-    genApiModule: async () => {
-      return apis;
-    },
-  });
-
-  const clientRPC = await createRPC<typeof apis>('apiConsumer', {
-    remoteCall: async (method, data) => {
-      const result = await serverRPC.RC(method, data);
-      return result;
-    },
-  });
-
-}
-main();
---
+++ apps/rpc/src/test.ts
@@ -0,0 +1,22 @@
+import { createRPC } from './createRPC';
+
+async function main() {
+  const apis = {
+    test() {
+      return 'Hello, world!';
+    },
+  };
+  const serverRPC = await createRPC('apiProvider' as const, {
+    genApiModule: async () => {
+      return apis;
+    },
+  });
+
+  const clientRPC = await createRPC<typeof apis>('apiConsumer', {
+    remoteCall: async (method, data) => {
+      const result = await serverRPC.RC(method, data);
+      return result;
+    },
+  });
+}
+main();
