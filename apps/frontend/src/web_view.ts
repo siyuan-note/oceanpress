e2d63d43cea6aad642d638eed9cb2735cc91a27b
崮生（子虚） <2234839456@qq.com>
2025-03-21 18:03:51 +0800

    添加编译为组件模式和 npm 模式

--- apps/frontend/src/web_view.ts
+++ apps/frontend/src/web_view.ts
@@ -1,10 +1,9 @@
-import '~/util/store.bower.dep.ts'
 import '~/core/render.api.dep.ts'
+import '~/util/store.bower.dep.ts'
 
-import { createApp, watchEffect } from 'vue'
+import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from '~/pages/App.tsx'
-import { swPrefix } from './sw/const.ts'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -16,27 +15,28 @@ const app = createApp(App)
 app.use(router)
 app.mount('#app')
 
-navigator.serviceWorker
-  /** 开发模式下 Firefox 还不支持 module，只能在 chrome 系下调试 */
-  .register(import.meta.env.PROD ? 'sw.iife.js' : '/oceanpress_preview/sw.ts', {
-    scope: swPrefix,
-    // 使用了 import 语句的必须要是 module 类型。
-    type: import.meta.env.PROD ? 'classic' : 'module',
-  })
-  .then(async (sw: ServiceWorkerRegistration) => {
-    await sw.update() // 当之前缓存的 sw 与现有的存在不一致时会重新安装
-    watchEffect(async () => {
-      // await (
-      //   await fetch(swConfigsPrefix, {
-      //     body: JSON.stringify(configs),
-      //     method: 'POST',
-      //   })
-      // ).json()
-    })
-  })
-  .catch((e) => {
-    console.log('sw 注册失败', e)
-  })
+// 暂时屏蔽 sw 这个功能
+// navigator.serviceWorker
+//   /** 开发模式下 Firefox 还不支持 module，只能在 chrome 系下调试 */
+//   .register(import.meta.env.PROD ? 'sw.iife.js' : '/oceanpress_preview/sw.ts', {
+//     scope: swPrefix,
+//     // 使用了 import 语句的必须要是 module 类型。
+//     type: import.meta.env.PROD ? 'classic' : 'module',
+//   })
+//   .then(async (sw: ServiceWorkerRegistration) => {
+//     await sw.update() // 当之前缓存的 sw 与现有的存在不一致时会重新安装
+//     watchEffect(async () => {
+//       // await (
+//       //   await fetch(swConfigsPrefix, {
+//       //     body: JSON.stringify(configs),
+//       //     method: 'POST',
+//       //   })
+//       // ).json()
+//     })
+//   })
+//   .catch((e) => {
+//     console.log('sw 注册失败', e)
+//   })
 
 if (import.meta.env.PROD) {
   // https://clarity.microsoft.com 站点分析
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,21 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type BuildEnvironmentOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
 import devtools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path/posix'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [devtools(), vue(), vueJSX()],
-  build: {
-    outDir: './dist/',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '~': '/src',
+export default defineConfig(({ mode }) => {
+  const isLibraryMode = mode === 'library'
+  console.log('[isLibraryMode]', isLibraryMode)
+  const buildConfig = (
+    isLibraryMode
+      ? {
+          lib: {
+            entry: {
+              app: resolve(__dirname, 'src/pages/App.tsx'),
+            },
+            fileName: (format, entryName) => `${entryName}.${format}.js`, // 根据入口名称生成文件名
+            name: 'OceanPressApp', // 库的全局变量名
+          },
+          rollupOptions: {
+            external: ['vue', 'fs/promises',"node:path","node:fs"],
+            output: {
+              globals: {
+                vue: 'Vue',
+              },
+            },
+          },
+        }
+      : {
+          outDir: './dist/',
+          emptyOutDir: true,
+        }
+  ) satisfies BuildEnvironmentOptions
+  return {
+    plugins: [devtools(), vue(), vueJSX()],
+    build: buildConfig,
+    resolve: {
+      alias: {
+        '~': '/src',
+      },
     },
-  },
-  server: {
-    port: 8080,
-  },
-  base: './',
+    server: {
+      port: 8080,
+    },
+    base: './',
+  }
 })
